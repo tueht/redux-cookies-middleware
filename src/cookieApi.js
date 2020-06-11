8be1bd958208edfdac6f3c6fe700b73e8a491c33
Hoang Trong Tue <tueht@users.noreply.github.com>
2020-06-12 05:30:29 +0700

    add httpOnly and sameSite config

--- src/cookieApi.js
+++ src/cookieApi.js
@@ -2,8 +2,8 @@ import jsCookie from 'js-cookie';
 
 const getCookie = name => jsCookie.get(name);
 
-const setCookie = (name, value, expiry = 365, secure = false) => {
-    jsCookie.set(name, value, { expires: expiry, path: '/', secure });
+const setCookie = (name, value, expiry = 365, secure = false, httpOnly = false, sameSite = 'Strict') => {
+    jsCookie.set(name, value, { expires: expiry, path: '/', secure, httpOnly, sameSite });
 };
 
 export { getCookie };

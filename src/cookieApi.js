e86e6dd2975f77700b5cf96f9e3f1cdef3c4b045
Hoang Trong Tue <tueht@users.noreply.github.com>
2020-06-12 05:46:23 +0700

    Update cookieApi.js

--- src/cookieApi.js
+++ src/cookieApi.js
@@ -2,8 +2,8 @@ import jsCookie from 'js-cookie';
 
 const getCookie = name => jsCookie.get(name);
 
-const setCookie = (name, value, expiry = 365, secure = false, httpOnly = false, sameSite = 'Strict') => {
-    jsCookie.set(name, value, { expires: expiry, path: '/', secure, httpOnly, sameSite });
+const setCookie = (name, value, expiry = 365, secure = false, sameSite = 'strict') => {
+    jsCookie.set(name, value, { expires: expiry, path: '/', secure, sameSite });
 };
 
 export { getCookie };
